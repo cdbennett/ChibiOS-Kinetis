92e0ef357e459a1ca25a8cfde086edfd360a701a
Colin D Bennett <colin@gibibit.com>
2013-08-15 13:15:05 -0700

    KL2x DMA low level M2M transfer test
    Transfers a block of data from one RAM location to another.
    Uses direct access to the KL2x DMA peripheral registers.

--- demos/ARMCM0P-FREEDOM-KL25Z/main.c
+++ demos/ARMCM0P-FREEDOM-KL25Z/main.c
@@ -17,6 +17,12 @@
 #include "ch.h"
 #include "hal.h"
 #include "test.h"
+#include "chprintf.h"
+
+static void dmaTest(BaseSequentialStream* log);
+static void dmaTestShowBuffers(BaseSequentialStream* log);
+static void dmaTestStart(BaseSequentialStream* log);
+
 
 static volatile int baseBlinkTime = 300;
 
@@ -80,6 +86,11 @@ int main(void) {
   chThdCreateStatic(waThread2, sizeof(waThread2), NORMALPRIO, Thread2, NULL);
   chThdCreateStatic(waThread3, sizeof(waThread3), NORMALPRIO, Thread3, NULL);
 
+
+  /// XXX CDB DMA test here
+  dmaTest((BaseSequentialStream*) &SD1);
+
+
   TestThread(&SD1);
 
   // Slow down the blink time to indicate test is completed.
@@ -88,4 +99,63 @@ int main(void) {
   while (TRUE) {
     chThdSleepMilliseconds(500);
   }
+}
+
+
+//
+// DMA TEST
+//
+
+#define DMA_TEST_BUF_SIZE 72
+static uint8_t srcbuf[DMA_TEST_BUF_SIZE] = "Hello, World! The quick brown fox jumped over the lazy dog.~~~~~~~~~~~*";
+static uint8_t dstbuf[DMA_TEST_BUF_SIZE] = "0123456789A123456789B123456789C123456789D123456789E123456789F123456789G";
+//                                       ^0        ^10       ^20       ^30       ^40       ^50       ^60       ^70 ([71]=\0)
+
+static void dmaTest(BaseSequentialStream* log)
+{
+    chprintf(log, "*** CDB starting DMA test\r\n");
+    chprintf(log, "Before DMA operation:\r\n");
+    dmaTestShowBuffers(log);
+    chprintf(log, "\r\n");
+    chprintf(log, "Sleeping a moment...\r\n");
+    chThdSleepMilliseconds(2000);
+    chprintf(log, "Initiating DMA operation\r\n");
+    dmaTestStart(log);
+    chprintf(log, "Sleeping a moment...\r\n");
+    chThdSleepMilliseconds(2000);
+    chprintf(log, "After DMA operation:\r\n");
+    dmaTestShowBuffers(log);
+    chprintf(log, "Sleeping a moment...\r\n");
+    chThdSleepMilliseconds(2000);
+    chprintf(log, "*** CDB done with DMA test\r\n");
+}
+
+static void dmaTestShowBuffers(BaseSequentialStream* log)
+{
+chprintf(log, "src buffer: '");
+chSequentialStreamWrite(log, srcbuf, DMA_TEST_BUF_SIZE);
+chprintf(log, "'\r\n");
+chprintf(log, "dst buffer: '");
+chSequentialStreamWrite(log, dstbuf, DMA_TEST_BUF_SIZE);
+chprintf(log, "'\r\n");
+}
+
+static void dmaTestStart(BaseSequentialStream* log)
+{
+    // Set up a DMA transfer from srcbuf to dstbuf.
+
+    DMA->ch[0].DSR_BCR = DMA_DSR_BCRn_DONE;    // must clear DONE (w1c) before starting
+    DMA->ch[0].SAR = (uint32_t) &srcbuf;
+    DMA->ch[0].DAR = (uint32_t) &dstbuf;
+    DMA->ch[0].DSR_BCR = DMA_DSR_BCRn_BCR(DMA_TEST_BUF_SIZE);
+    DMA->ch[0].DCR =
+            DMA_DCRn_SINC       // increment src
+            | DMA_DCRn_SSIZE(1) // SSIZE=01 (8-bit transfer size)
+            | DMA_DCRn_DINC     // increment dst
+            | DMA_DCRn_DSIZE(1) // DSIZE=01 (8-bit transfer size)
+            | DMA_DCRn_START    // START=1 to initiate DMA request
+            ;
+
+    // Don't need to use DMAMUX for software initiated DMA request.
+    // DMAMUX->CHCFG[0] = ...;
 }
